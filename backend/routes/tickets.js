a35074eecf3f1c71a2dc357497912a8b3670a68a
Pooja <2309poojamurugan@gmail.com>
2026-01-28 11:18:59 +0530

    Initial commit

---
+++ backend/routes/tickets.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createTicket,
+  getUserTickets,
+  getTicketById,
+} = require("../controllers/ticketsController");
+const { auth, userAuth } = require("../middleware/auth");
+
+// All routes require authentication
+router.use(auth);
+
+// User routes - require user role
+router.post("/", userAuth, createTicket);
+router.get("/user/:id", getUserTickets);
+router.get("/:id", getTicketById);
+
+module.exports = router;
